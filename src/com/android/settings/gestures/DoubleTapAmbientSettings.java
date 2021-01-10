05772d74056b097c3d8b5230e1150d68b43a6190
SKULSHADY <anushekprasal@gmail.com>
2021-01-10 15:30:33 +0000

    Settings: Double tap to trigger doze [2/2]
    * Also adapt Add video to Double tap to trigger doze from Pie: a66601a
    
    Change-Id: Id98543542db937c6cc1f176cd55f6b02e472849d

---
+++ src/com/android/settings/gestures/DoubleTapAmbientSettings.java
@@ -0,0 +1,31 @@
+package com.android.settings.gestures;
+
+import android.os.Bundle;
+
+import androidx.preference.Preference;
+
+import com.android.internal.logging.nano.MetricsProto.MetricsEvent;
+import com.android.settings.R;
+import com.android.settings.SettingsPreferenceFragment;
+
+public class DoubleTapAmbientSettings extends SettingsPreferenceFragment implements
+        Preference.OnPreferenceChangeListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.double_tap_ambient_screen_settings);
+
+        getActivity().getActionBar().setTitle(R.string.double_tap_title);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        return false;
+    }
+
+    @Override
+    public int getMetricsCategory() {
+        return MetricsEvent.CLOVER;
+    }
+}
