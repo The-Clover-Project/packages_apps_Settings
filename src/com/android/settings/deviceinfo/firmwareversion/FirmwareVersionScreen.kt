8b41d7f149ac9ef4877db6a36886a91e72636d40
Ritesh <SahanyRitesh@proton.me>
2025-11-20 09:39:27 +0530

    Settings: Add Clover Maintainer Preference
    Change-Id: Ifc6e2ddfa84096566e277c59350388252afdbaf8

--- src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
+++ src/com/android/settings/deviceinfo/firmwareversion/FirmwareVersionScreen.kt
@@ -71,6 +71,7 @@ open class FirmwareVersionScreen : PreferenceScreenMixin, PreferenceSummaryProvi
             +FirmwareVersionDetailPreference()
             +CloverVersionPreference()
             +SecurityPatchLevelPreference()
+            +CloverMaintainerPreference()
             +BasebandVersionPreference()
             +KernelVersionPreference()
             +SimpleBuildNumberPreference()
